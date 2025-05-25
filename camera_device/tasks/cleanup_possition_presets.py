7f75ef7ad488174873881004545ba6c7415de5ef
Nikhila C <115739037+NikhilA8606@users.noreply.github.com>
2025-05-25 19:55:07 +0530

    Add cron job task to cleanup up `PositionPreset` objects for deleted FacilityLocation / Camera Device (#13)

---
+++ camera_device/tasks/cleanup_possition_presets.py
@@ -0,0 +1,21 @@
+from logging import Logger
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.db.models import Q
+
+from camera_device.models import PositionPreset
+
+logger: Logger = get_task_logger(__name__)
+
+
+@shared_task
+def cleanup_possition_presets():
+    """
+    Deletes PositionPreset objects whose associated FacilityLocation or Camera device have been deleted.
+    """
+    logger.info("Cleaning up orphaned PositionPreset objects")
+    queryset = PositionPreset.objects.filter(
+        Q(location__deleted=True) | Q(camera__deleted=True)
+    )
+    queryset.update(deleted=True)

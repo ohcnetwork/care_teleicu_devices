3bb523cf17212fe4d3908babaf1c5e5ef951fe9d
Aakash Singh <mail@singhaakash.dev>
2025-11-05 11:45:37 +0530

    add open id views

--- gateway_device/urls.py
+++ gateway_device/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
+from gateway_device.viewsets.open_id import PublicJWKsView
+
 router = DefaultRouter()
+
+router.register("", PublicJWKsView, basename="public-jwks")
 
 urlpatterns = router.urls

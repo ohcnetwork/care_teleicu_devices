d700b6a980fbf52d0ae2c541eaad50c470c58ae5
rithviknishad <mail@rithviknishad.dev>
2025-11-30 14:29:59 +0530

    fix import

--- vitals_observation_device/urls.py
+++ vitals_observation_device/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from plugins.care_teleicu_devices.vitals_observation_device.viewsets.automated_observations import (
+from vitals_observation_device.viewsets.automated_observations import (
     AutomatedObservationsViewSet,
 )
 

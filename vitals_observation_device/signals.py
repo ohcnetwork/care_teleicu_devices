09ab89ca48e5628e090bbe6d5519a211545c457f
rithviknishad <mail@rithviknishad.dev>
2025-12-01 17:47:22 +0530

    fix encounter history when unlinking devices on location change

--- vitals_observation_device/signals.py
+++ vitals_observation_device/signals.py
@@ -1,11 +1,12 @@
 from django.db.models.signals import post_save
+from django.db import transaction
 from django.dispatch import receiver
 
-from care.emr.models import FacilityLocation, Device
+from care.emr.models import FacilityLocation, Device, DeviceEncounterHistory
 
 
 @receiver(post_save, sender=FacilityLocation)
-def unlink_on_encounter_location_end(sender, instance, created, **kwargs):
+def unlink_on_encounter_location_changed(sender, instance, created, **kwargs):
     """
     Signal handler to unlink vitals observation device from the encounter
     when the bed (location) is no longer associated with the same encounter
@@ -14,4 +15,14 @@ def unlink_on_encounter_location_end(sender, instance, created, **kwargs):
     devices_to_unlink = Device.objects.filter(
         care_type="vitals-observation", current_location=instance
     ).exclude(current_encounter=instance.current_encounter)
-    devices_to_unlink.update(current_encounter=None)
+
+    with transaction.atomic():
+        for device in devices_to_unlink:
+            if device.current_encounter:
+                old_obj = DeviceEncounterHistory.objects.filter(
+                    device=device, encounter=device.current_encounter, end__isnull=True
+                ).first()
+                if old_obj:
+                    old_obj.end = device.current_encounter.date_of_admission
+                    old_obj.save()
+        devices_to_unlink.update(current_encounter=None)
